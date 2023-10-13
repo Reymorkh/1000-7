8ce2fcf774aca28cd879f60cba8307500283a40f
Reymorkh <aareimorkh@edu.hse.ru>
2023-10-13 20:19:03 +0500

    Добавьте файлы проекта.

---
+++ 1000-7/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace _1000_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            OkButton = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // OkButton
+            // 
+            OkButton.Location = new Point(70, 60);
+            OkButton.Name = "OkButton";
+            OkButton.Size = new Size(80, 25);
+            OkButton.TabIndex = 0;
+            OkButton.Text = "-7?";
+            OkButton.UseVisualStyleBackColor = true;
+            OkButton.Click += OkButton_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(59, 31);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(204, 111);
+            Controls.Add(textBox1);
+            Controls.Add(OkButton);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "1000-7?";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button OkButton;
+        private TextBox textBox1;
+    }
+}
---
+++ 1000-7/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System.DirectoryServices.ActiveDirectory;
+using static System.Windows.Forms.LinkLabel;
+
+namespace _1000_7
+{
+    public partial class Form1 : Form
+    {
+        string line;
+        Random ded = new Random();
+        List<string> spisok = new List<string>() { "deadinside✓", "emo✓", "drain✓", "epileptic✓", "paranoid✓", "toxic✓", "bipolar✓", "depressed✓", "tilted✓", "antisocial✓", "sad✓", "broken✓", "aggressive✓", "psycho✓", "apathetic✓" };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (!File.Exists("1000-7.txt"))
+            {
+                var stream = File.Create("1000-7.txt");
+                stream.Close();
+                File.WriteAllText("1000-7.txt", "1000");
+            }
+
+            StreamReader sr = new StreamReader("1000-7.txt");
+            line = sr.ReadLine();
+            sr.Close();
+
+            textBox1.Text = line;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            int i = ded.Next(0, 14);
+
+            MessageBox.Show(spisok[i], "");
+            File.WriteAllText("1000-7.txt", Convert.ToString(Convert.ToInt32(line) - 7));
+            StreamReader sr = new StreamReader("1000-7.txt");
+            line = sr.ReadLine();
+            sr.Close();
+            textBox1.Text = line;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
